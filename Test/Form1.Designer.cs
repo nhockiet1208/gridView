a5ad23f0d977341defa7276cdd2ea743ee4c7311
Kiet Hoang <minhchuc311@gmail.com>
2021-04-06 11:42:39 -0700

    GridView

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridViewEmployee = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colOccupation = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colPhone = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDOB = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colGender = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemDateEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemDateEdit();
+            this.repositoryItemLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewEmployee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemDateEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemDateEdit1.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridViewEmployee;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemDateEdit1,
+            this.repositoryItemLookUpEdit1});
+            this.gridControl1.Size = new System.Drawing.Size(914, 392);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridViewEmployee});
+            // 
+            // gridViewEmployee
+            // 
+            this.gridViewEmployee.Appearance.GroupPanel.BackColor = System.Drawing.Color.Transparent;
+            this.gridViewEmployee.Appearance.GroupPanel.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gridViewEmployee.Appearance.GroupPanel.ForeColor = System.Drawing.Color.Black;
+            this.gridViewEmployee.Appearance.GroupPanel.Options.UseBackColor = true;
+            this.gridViewEmployee.Appearance.GroupPanel.Options.UseFont = true;
+            this.gridViewEmployee.Appearance.GroupPanel.Options.UseForeColor = true;
+            this.gridViewEmployee.Appearance.GroupPanel.Options.UseTextOptions = true;
+            this.gridViewEmployee.Appearance.GroupPanel.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridViewEmployee.Appearance.HeaderPanel.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gridViewEmployee.Appearance.HeaderPanel.Options.UseFont = true;
+            this.gridViewEmployee.Appearance.OddRow.BackColor = System.Drawing.Color.Silver;
+            this.gridViewEmployee.Appearance.OddRow.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gridViewEmployee.Appearance.OddRow.Options.UseBackColor = true;
+            this.gridViewEmployee.Appearance.OddRow.Options.UseFont = true;
+            this.gridViewEmployee.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colID,
+            this.colName,
+            this.colOccupation,
+            this.colPhone,
+            this.colDOB,
+            this.colGender});
+            this.gridViewEmployee.GridControl = this.gridControl1;
+            this.gridViewEmployee.GroupPanelText = "Danh Sach Nhan Vien";
+            this.gridViewEmployee.Name = "gridViewEmployee";
+            this.gridViewEmployee.OptionsSelection.MultiSelect = true;
+            this.gridViewEmployee.OptionsView.EnableAppearanceOddRow = true;
+            // 
+            // colID
+            // 
+            this.colID.Caption = "Employee ID";
+            this.colID.MinWidth = 25;
+            this.colID.Name = "colID";
+            this.colID.Visible = true;
+            this.colID.VisibleIndex = 0;
+            this.colID.Width = 94;
+            // 
+            // colName
+            // 
+            this.colName.Caption = "Name";
+            this.colName.MinWidth = 25;
+            this.colName.Name = "colName";
+            this.colName.Visible = true;
+            this.colName.VisibleIndex = 1;
+            this.colName.Width = 94;
+            // 
+            // colOccupation
+            // 
+            this.colOccupation.Caption = "Occupation";
+            this.colOccupation.ColumnEdit = this.repositoryItemLookUpEdit1;
+            this.colOccupation.MinWidth = 25;
+            this.colOccupation.Name = "colOccupation";
+            this.colOccupation.Visible = true;
+            this.colOccupation.VisibleIndex = 2;
+            this.colOccupation.Width = 94;
+            // 
+            // colPhone
+            // 
+            this.colPhone.Caption = "Phone Number";
+            this.colPhone.MinWidth = 25;
+            this.colPhone.Name = "colPhone";
+            this.colPhone.Visible = true;
+            this.colPhone.VisibleIndex = 3;
+            this.colPhone.Width = 94;
+            // 
+            // colDOB
+            // 
+            this.colDOB.Caption = "D.O.B";
+            this.colDOB.ColumnEdit = this.repositoryItemDateEdit1;
+            this.colDOB.MinWidth = 25;
+            this.colDOB.Name = "colDOB";
+            this.colDOB.Visible = true;
+            this.colDOB.VisibleIndex = 4;
+            this.colDOB.Width = 94;
+            // 
+            // colGender
+            // 
+            this.colGender.Caption = "Gender";
+            this.colGender.MinWidth = 25;
+            this.colGender.Name = "colGender";
+            this.colGender.Visible = true;
+            this.colGender.VisibleIndex = 5;
+            this.colGender.Width = 94;
+            // 
+            // repositoryItemDateEdit1
+            // 
+            this.repositoryItemDateEdit1.AutoHeight = false;
+            this.repositoryItemDateEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemDateEdit1.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemDateEdit1.Name = "repositoryItemDateEdit1";
+            // 
+            // repositoryItemLookUpEdit1
+            // 
+            this.repositoryItemLookUpEdit1.AutoHeight = false;
+            this.repositoryItemLookUpEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemLookUpEdit1.Name = "repositoryItemLookUpEdit1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 440);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewEmployee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemDateEdit1.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemDateEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridViewEmployee;
+        private DevExpress.XtraGrid.Columns.GridColumn colID;
+        private DevExpress.XtraGrid.Columns.GridColumn colName;
+        private DevExpress.XtraGrid.Columns.GridColumn colOccupation;
+        private DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit repositoryItemLookUpEdit1;
+        private DevExpress.XtraGrid.Columns.GridColumn colPhone;
+        private DevExpress.XtraGrid.Columns.GridColumn colDOB;
+        private DevExpress.XtraEditors.Repository.RepositoryItemDateEdit repositoryItemDateEdit1;
+        private DevExpress.XtraGrid.Columns.GridColumn colGender;
+    }
+}
+
